60a5718eedd9ee1321293cb324cccd05d7aa32e9
EvanCopas <evancopas@gmail.com>
2023-06-30 15:13:16 -0400

    bleh

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ let navHome = document.querySelector("#home");
 let navAboutMe = document.querySelector("#aboutme");
 let navProjects = document.querySelector("#projects");
 let navContact = document.querySelector("#contact");
-
+let navEducation = document.querySelector("#education");
 let homePage = document.querySelector("#homePage");
 let aboutmePage = document.querySelector("#aboutmePage");
 let projectsPage = document.querySelector("#projectsPage");
 let contactPage = document.querySelector("#contactPage");
+let educationPage = document.querySelector("#educationPage");
 
 navHome.onclick = function () {
     homePage.style.display = "";
     aboutmePage.style.display = "none";
     projectsPage.style.display = "none";
+    educationPage.style.display = "none";
     contactPage.style.display = "none";
 }
 
@@ -19,6 +21,7 @@ navAboutMe.onclick = function () {
     homePage.style.display = "none";
     aboutmePage.style.display = "flex";
     projectsPage.style.display = "none";
+    educationPage.style.display = "none";
     contactPage.style.display = "none";
 }
 
@@ -26,6 +29,15 @@ navProjects.onclick = function () {
     homePage.style.display = "none";
     aboutmePage.style.display = "none";
     projectsPage.style.display = "block";
+    educationPage.style.display = "none";
+    contactPage.style.display = "none";
+}
+
+navEducation.onclick = function () {
+    homePage.style.display = "none";
+    aboutmePage.style.display = "none";
+    projectsPage.style.display = "none";
+    educationPage.style.display = "block";
     contactPage.style.display = "none";
 }
 
@@ -33,5 +45,6 @@ navContact.onclick = function () {
     homePage.style.display = "none";
     aboutmePage.style.display = "none";
     projectsPage.style.display = "none";
+    educationPage.style.display = "none";
     contactPage.style.display = "block";
 }
